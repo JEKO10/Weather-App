f37cde8ff72ac3e6bfec51319f6a90b5e21dda4f
Aleksa Bubanja <aleksa3b@gmail.com>
2021-09-28 14:05:34 +0200

    fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const api = {
+  key: "30eb85b09f9584e4d9c77aee8b3aaf05",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [weather, setWeather] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${api.base}weather?q=London&appid=${api.key}`
+      );
+      const data = await response.json();
+      setWeather(data);
+      console.log(data);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <h1>ALerks</h1>
+      <h1></h1>
     </div>
   );
 }
